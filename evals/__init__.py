0d24738f5d33a9f8be671e91e39bf2ca8dd17ce8
Amir <amir83f14@yahoo.com>
2026-02-28 18:59:21 -0800

    wire eval metrics to runner

--- evals/__init__.py
+++ evals/__init__.py
@@ -2,13 +2,23 @@
 
 from meal_analysis.schemas import EvalSample
 from evals.metrics import compute_metrics, p50_latency_ms, run_level_composite, score_guardrails, score_meal, score_safety
-from evals.runner import discover_pairs, load_ground_truth, run_all, run_one, write_results
+from evals.runner import (
+    compute_metrics_from_file,
+    discover_pairs,
+    load_ground_truth,
+    load_results,
+    run_all,
+    run_one,
+    write_results,
+)
 
 __all__ = [
     "EvalSample",
     "compute_metrics",
+    "compute_metrics_from_file",
     "discover_pairs",
     "load_ground_truth",
+    "load_results",
     "p50_latency_ms",
     "run_all",
     "run_level_composite",
